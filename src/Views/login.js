15e533e926ff8c7394a06010a001cb04f9bb3206
seiks <mauricio25luna@gmail.com>
2020-12-08 08:54:15 -0600

    visual fix

--- src/Views/login.js
+++ src/Views/login.js
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
-import { Container, Header, Content, Form, Item, Input, Label, Button, Text } from 'native-base';
+import { Container, Header, Content, Form, Item, Input, Label, Button, Text ,Thumbnail } from 'native-base';
+import { Col, Row, Grid } from 'react-native-easy-grid';
 
 export default Login = ({ navigation }) => {
-
+  const uri = "https://i.pinimg.com/originals/0c/3b/3a/0c3b3adb1a7530892e55ef36d3be6cb8.png";
   const [name, setName] = useState("")
   return (
     <Container>
       <Header />
-      <Content>
+      <Content contentContainerStyle={{flex: 1}} style={{padding: 10}}> 
+        <Grid style={{alignItems: 'center'}}>
+        <Row style={{ flex: 1, flexDirection: 'row',alignItems: 'center'}}>
+          <Col style={{ alignItems: 'center'}}>
+          <Thumbnail large source={{uri: uri}} style={{ height: 200,width:200}}/>
+          <Text>Epub Reader</Text>
+          </Col>
+        </Row>   
+        <Row style={{ flex: 1, flexDirection: 'row'}}>
+          <Col >
         <Form>
           <Item inlineLabel>
             <Label>Username</Label>
@@ -25,6 +35,9 @@ export default Login = ({ navigation }) => {
             <Text> Sign In </Text>
           </Button>
         </Form>
+        </Col>
+        </Row>
+        </Grid>
       </Content>
     </Container>
   );
