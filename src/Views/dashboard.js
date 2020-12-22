eed4da7f42cd8ac3a08ed23deff1a909a0471a3a
dev503 <seikstest@gmail.com>
2020-12-22 01:47:50 -0600

    Reestructura y unificando a hooks (arrow function)

--- src/Views/dashboard.js
+++ src/Views/dashboard.js
@@ -1,14 +1,15 @@
 import React from 'react';
+// import {  DrawerLayoutAndroid, StyleSheet, View } from "react-native";
 import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
-import Drawer from '../Components/drawer'
+import AppHeader from './drawer';
 //import EpubReader from '@sbrighiu/epubjs-rn';
 
-export default AnatomyExample = ({navigation, route}) => {  
+export default Anatomy = ({navigation, route}) => {  
 
     return (
       <Container>
 
-          <Drawer />
+          <AppHeader> </AppHeader>
 
       </Container>
     );
--- src/Views/drawer.js
+++ src/Views/drawer.js
@@ -1,7 +1,24 @@
-import React, { Component } from 'react'
-import { Text } from 'react-native'
-import { Header, Left, Button, Icon, Right, Body, Title, Drawer, Content,Footer, FooterTab  } from 'native-base'
-import SideBar from './sidebar'
+import React, { Component } from 'react';
+// import { Text } from 'react-native';
+import {Header, Text, Left, Button, Icon, Right, Body, Title, Drawer, Content,Footer, FooterTab  } from 'native-base'
+import SideBar from '../Components/sidebar'
+// import HeaderCont from '../Components/header'
+
+// const openDrawer () => {
+//    this.drawer._root.open() 
+//   };
+
+// render() { 
+//   return ( 
+//   <Drawer ref={(ref) => { this.drawer = ref; }} 
+//   content={
+//   <SideBar navigator={
+//     this.navigator} />} onClose={() => this.closeDrawer()} >
+//        // Main View 
+//        </Drawer> ); 
+//        } 
+//       }
+
 
 export default class AppHeader extends Component {
   closeDrawer() {
@@ -15,9 +32,9 @@ export default class AppHeader extends Component {
       <Drawer
        ref={(ref) => { this.drawer = ref; }}
         content={<SideBar />}
-        onClose={() => this.closeDrawer()}
-  
-      >
+        onClose={() => this.closeDrawer()}>
+        {/* <HeaderCont>
+          </HeaderCont> */}
         <Header>
           <Left>
             <Button transparent onPress={() => this.openDrawer()}>
@@ -25,11 +42,11 @@ export default class AppHeader extends Component {
             </Button>
           </Left>
      
-          <Right>
+          {/* <Right>
             <Button transparent>
               <Icon name="bulb" />
             </Button>
-          </Right>
+          </Right> */}
         </Header>
 
         <Content >
