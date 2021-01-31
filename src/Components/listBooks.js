9c16f66d87f3b520e686d56e35f9f70323f122d7
seiks <mauricio25luna@gmail.com>
2021-01-31 08:21:02 -0600

    test cmmit

---
+++ src/Components/listBooks.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+export default class Camiseta extends Component {
+
+
+render(){
+     return(
+            <View>
+               <Text>Color de la camiseta: </Text>
+               <Text>Tejido:</Text>
+            </View>
+        )
+    }
+}
--- src/Views/dashboard.js
+++ src/Views/dashboard.js
@@ -1,5 +1,7 @@
 import React, { useRef } from 'react';
-import { DrawerLayoutAndroid } from 'react-native';
+import { DrawerLayoutAndroid,FlatList,SafeAreaView,View } from 'react-native';
+import * as axios from 'axios';
+
 import {
   Container,
   Content,
@@ -15,17 +17,75 @@ import {
   Text,
 } from 'native-base';
 import Drawer from './drawer';
+var Data2;
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
 
-const Dashboard = ({ navigation }) => {
-  const drawer = useRef(null);
+const Item = ({ title }) => (
+  <View >
+    <Text>{title}</Text>
+  </View>
+);
+  // Función para mandar datos del login
+  const getBooks = () => {
+    axios
+      .post(
+        'http://backoffice.moondevsv.com/Backend/public/books/list',
+        {
+          token: '12345'
+        },
+      )
+      .then((res) => {
+       // console.log(res.data)
+        //Data2 = res.data
+       // return res.data;
+        //totalData = res.data
+        /*res.data.forEach(element => {
+          
+        });*/
+        //return res.data
+        //return res
+        console.log("-----------------------------------------")
+      // var  data = JSON.parse(res.data) 
+        console.log(res.data['data'][0]["title"])
+        return res.data['data']
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+
+  
+
+  const Dashboard = ({ navigation }) => {
+
+    const renderItem = ({ item }) => (
+      <Item title={item["title"]} />
+    );
+    //const Data2 = getBooks()
+  
   return (
     <DrawerLayoutAndroid
-      ref={drawer}
+      ref={Drawer}
       drawerWidth={300}
       drawerPosition="right"
       renderNavigationView={() => {
         return <Drawer navigation={navigation} />;
       }}>
+        
       <Container>
         <Header>
           <Left />
@@ -43,6 +103,13 @@ const Dashboard = ({ navigation }) => {
           <Button onPress={() => navigation.navigate('Reader')}>
             <Text>Show reader</Text>
           </Button>
+          <SafeAreaView>
+            <FlatList 
+              data={getBooks()}
+              renderItem={renderItem}
+              keyExtractor={item => item["id"]}
+              />
+          </SafeAreaView>
         </Content>
         <Footer>
           <FooterTab>
